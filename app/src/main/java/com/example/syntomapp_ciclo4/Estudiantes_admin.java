e014cf02eb0a551b57332acef92e3c87899d049b
cristian cardenas <cristian.cardenas1.mt@usa.edu.co>
2022-12-19 07:10:43 -0500

    Primer commit

---
+++ app/src/main/java/com/example/syntomapp_ciclo4/Estudiantes_admin.java
@@ -0,0 +1,14 @@
+package com.example.syntomapp_ciclo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Estudiantes_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estudiantes_admin);
+    }
+}
---
+++ app/src/main/java/com/example/syntomapp_ciclo4/Modulo_Admin.java
@@ -0,0 +1,14 @@
+package com.example.syntomapp_ciclo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Modulo_Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modulo_admin);
+    }
+}
